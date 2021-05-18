8bfc193ad65ffa6bc59fb3b87b2a6e315970b51f
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-05-18 13:21:12 +0200

    20 minutos y 3 intentos

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,22 @@
 function numPrimo(input) {
- return null;
+    if(input === 1)
+        return -1;
+
+    let numerosPrimos = [];
+    
+    for (let i= 1; i <= input; i++) {
+		if (esprimo(i)) 
+            numerosPrimos.push(i);
+	}
+    return numerosPrimos;
+}
+
+function esprimo(num){
+    if (num == 0 || num == 1 || num == 4) return false;
+	for (let x = 2; x < num / 2; x++) {
+		if (num % x == 0) return false;
+	}
+	return true;
 }
 
 module.exports = numPrimo;
